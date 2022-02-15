a3304f5c491a6a712723757e4ea76b85a7d837a0
Alex Al <alexsputnik@gmail.com>
2022-02-16 00:53:23 +0200

    solutionTree

---
+++ solution_test.go
@@ -0,0 +1,11 @@
+package solution
+
+import "testing"
+
+func TestSolution(t *testing.T) {
+	want := "Hello 🗺️ "
+
+	if got := GetMessage(); got != want {
+		t.Errorf("GetMessage() = %q, want %q", got, want)
+	}
+}
